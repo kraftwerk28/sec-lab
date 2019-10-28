52c281b98775ce578f870fe82d9e3ac1c84b04ff
Nikita Machekhin <nicitamac@gmail.com>
2019-10-29 01:15:17 +0200

    Add db provider

---
+++ packages/service-1/db.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: 'localhost',
+  port: 5432,
+  database: 'tickets_service_1',
+  user: 'postgres',
+  password: '790000'
+});
+
+const query = `
+  SELECT id_ticket, price, time, seat,
+    from_a.airport_name AS from_name,
+    from_a.country AS from_country,
+    from_a.city AS from_city,
+    to_a.airport_name AS to_name,
+    to_a.country AS to_country,
+    to_a.city AS to_city
+  FROM
+    ticket t JOIN airports from_a
+    ON t.from_airp = from_a.airport_id
+    JOIN airports to_a
+    ON t.to_airp = to_a.airport_id
+  WHERE
+    (from_a.airport_name = $1 OR $1 ISNULL) AND
+    (to_a.airport_name = $2 OR $2 ISNULL) AND
+    (time = $3 OR $3 ISNULL) AND
+    (price < $4 OR $4 ISNULL)
+`;
+
+const select = conditions => pool
+  .query(query, [
+    conditions.from,
+    conditions.to,
+    conditions.time,
+    conditions.maxPrice])
+  .then(data => data.rows);
+
+module.exports = select;
