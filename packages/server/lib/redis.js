60a1024f76ec4982ac91db524bd4dbe759497015
kraftwerk28 <kefirchik3@gmail.com>
2019-11-11 02:16:45 +0200

    add: Redis caching for service #1

--- packages/server/lib/redis.js
+++ packages/server/lib/redis.js
@@ -1,15 +1,52 @@
 import redis from 'redis'
+import { parse } from 'url'
+import { promisify } from 'util'
+import { registerCallback } from './gracefulShutdown'
 
 const client = redis.createClient()
 
-export const end = () =>
-  new Promise(res => {
-    client.quit(res)
-  })
+const rExists = promisify(client.exists.bind(client))
+const rGet = promisify(client.get.bind(client))
+const rSet = promisify(client.set.bind(client))
 
-client.set('lol', 'kek', () => {
-  client.set('abc', '123', () => {
-    client.keys('*', (...vals) => console.log(vals))
-    client.quit()
-  })
-})
+registerCallback(client.quit.bind(client), () =>
+  console.log('Redis disconnected...')
+)
+
+export const s1CheckCache = async (req, res, next) => {
+  res.type('application/json')
+  const { query } = req.query
+  if (!query) {
+    res.code(400).send()
+  }
+  const exists = await rExists(query)
+  if (exists) {
+    res.code(200).send(Buffer.from(await rGet(query)))
+  } else {
+    return next()
+  }
+}
+
+export const s1Cache = async (request, reply, res) => {
+  // retrieve key for lookup in Redis
+  const url = parse(request.url, true)
+  const { query } = url.query
+
+  reply.type('application/json').removeHeader('transfer-encoding')
+
+  const exists = await rExists(query)
+  if (exists) {
+    // pipe response to client, if cached
+    reply.code(200).send(res)
+  } else {
+    // otherwise cache result if not exists
+    let data = []
+    res
+      .on('data', ch => data.push(ch))
+      .on('end', () => {
+        const body = Buffer.concat(data)
+        rSet(query, body)
+        reply.code(200).send(body)
+      })
+  }
+}
--- packages/server/lib/server.js
+++ packages/server/lib/server.js
@@ -3,7 +3,8 @@ import fastifyProxy from 'fastify-http-proxy'
 
 import { registerPromise } from './gracefulShutdown'
 import { connect } from './db'
-import * as routes from './router'
+import * as r from './router'
+import * as redis from './redis'
 
 const app = fastify()
 
@@ -12,11 +13,12 @@ const { SERVICE1_PORT, SERVICE2_PORT } = process.env
 app.register(fastifyProxy, {
   upstream: `http://127.0.0.1:${SERVICE1_PORT}`,
   prefix: '/s1',
-  preHandler(req, res, next) {
-    console.log('Service 1 proxy...')
-    return next()
+  preHandler: redis.s1CheckCache,
+  replyOptions: {
+    onResponse: redis.s1Cache
   }
 })
+
 app.register(fastifyProxy, {
   upstream: `http://127.0.0.1:${SERVICE2_PORT}`,
   prefix: '/s2',
@@ -29,21 +31,16 @@ app.register(fastifyProxy, {
 const { SERVER_PORT } = process.env
 const PORT = SERVER_PORT || 8080
 
-app.get('/', routes.index)
-app.get('/get-airports', routes.getAirports)
-app.get('/get-tickets', routes.getTickets)
-app.get('/get-clients', routes.getClients)
-app.get('/get-flights', routes.getFlights)
+app.get('/', r.index)
+app.get('/get-airports', r.getAirports)
+app.get('/get-tickets', r.getTickets)
+app.get('/get-clients', r.getClients)
+app.get('/get-flights', r.getFlights)
 
-app.post('/add-airport', routes.addAirport)
-app.post('/add-ticket', routes.addTicket)
-app.post('/add-client', routes.addClient)
-app.post('/add-flight', routes.addFlight)
-
-app.get('/test/:id1/:id2', (req, res) => {
-  console.log(req.params, req.query)
-  res.status(200).send('ok')
-})
+app.post('/add-airport', r.addAirport)
+app.post('/add-ticket', r.addTicket)
+app.post('/add-client', r.addClient)
+app.post('/add-flight', r.addFlight)
 
 app
   .listen({ host: '0.0.0.0', port: PORT })
